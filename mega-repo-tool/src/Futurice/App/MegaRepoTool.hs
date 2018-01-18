3fce658bc7c865b4b61636e96688f21bc19207ce
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-01-18 22:45:55 +0200

    Merge tajna into mega-repo-tool

--- mega-repo-tool/src/Futurice/App/MegaRepoTool.hs
+++ mega-repo-tool/src/Futurice/App/MegaRepoTool.hs
@@ -13,17 +13,26 @@ import qualified Text.Microstache    as M
 import Futurice.App.MegaRepoTool.BuildDocker
 import Futurice.App.MegaRepoTool.Config
 import Futurice.App.MegaRepoTool.Estimator
+import Futurice.App.MegaRepoTool.Exec
+import Futurice.App.MegaRepoTool.GenPass
+import Futurice.App.MegaRepoTool.Keys
 import Futurice.App.MegaRepoTool.Scripts
 import Futurice.App.MegaRepoTool.Stats
 
-
 data Cmd
-    = BuildDocker [AppName]
-    | Action (IO ())
-    | ViewConfig
+    = CmdBuildDocker [AppName]
+    | CmdAction (IO ())
+    | CmdViewConfig
+    | CmdGenPass
+    | CmdListKeys !Bool
+    | CmdAddKey !Text !Text
+    | CmdAddOwnKey !Text !Text
+    | CmdUpdateKey !Text !Text
+    | CmdDeleteKey !Text
+    | CmdExec !(NonEmpty String)
 
 buildDockerOptions ::O.Parser Cmd
-buildDockerOptions = BuildDocker
+buildDockerOptions = CmdBuildDocker
     <$> some (O.strArgument $ mconcat
         [ O.metavar ":component"
         , O.help "Component/image to build"
@@ -36,29 +45,79 @@ buildDockerOptions = BuildDocker
     mk = map (view unpacked) . Map.keys . _mrtApps
 
 packdepsOptions :: O.Parser Cmd
-packdepsOptions = pure $ Action packdepsScript
+packdepsOptions = pure $ CmdAction packdepsScript
 
 dotOptions :: O.Parser Cmd
-dotOptions = pure $ Action dotScript
+dotOptions = pure $ CmdAction dotScript
 
 statsOptions :: O.Parser Cmd
-statsOptions =  pure $ Action stats
+statsOptions =  pure $ CmdAction stats
 
 estimatorOptions :: O.Parser Cmd
-estimatorOptions = fmap Action $ estimator
-    <$> O.strArgument (mconcat [ O.metavar ":file", O.help "TODO File" ])
+estimatorOptions = fmap CmdAction $ estimator
+    <$> strArgument [ O.metavar ":file", O.help "TODO File" ]
 
 viewConfigOptions :: O.Parser Cmd
-viewConfigOptions = pure ViewConfig
+viewConfigOptions = pure CmdViewConfig
+
+genPassOptions :: O.Parser Cmd
+genPassOptions = pure CmdGenPass
+
+listKeysOptions :: O.Parser Cmd
+listKeysOptions = CmdListKeys
+    <$> O.switch (mconcat [O.long "show-values", O.help "Show the values or keys"])
+
+addKeyOptions :: O.Parser Cmd
+addKeyOptions = CmdAddKey
+    <$> strArgument [ O.metavar ":key", O.help "Key identifier" ]
+    <*> strArgument [ O.metavar ":value", O.help "Key value" ]
+
+addOwnKeyOptions :: O.Parser Cmd
+addOwnKeyOptions = CmdAddOwnKey
+    <$> strArgument [ O.metavar ":key", O.help "Key identifier" ]
+    <*> strArgument [ O.metavar ":value", O.help "Key value" ]
+
+updateKeyOptions :: O.Parser Cmd
+updateKeyOptions = CmdUpdateKey
+    <$> strArgument [ O.metavar ":key", O.help "Key identifier" ]
+    <*> strArgument [ O.metavar ":value", O.help "Key value" ]
+
+deleteKeyOptions :: O.Parser Cmd
+deleteKeyOptions = CmdDeleteKey
+    <$> strArgument [ O.metavar ":key", O.help "Key identifier" ]
+
+runOptions :: O.Parser Cmd
+runOptions = cmdRun
+    <$> many (O.strArgument $ O.metavar ":arg" <> O.help "arguments")
+  where
+    cmdRun args = CmdExec ("cabal" :| "new-run" : args)
+
+execOptions :: O.Parser Cmd
+execOptions = mk 
+    <$> O.strArgument (O.metavar ":cmd" <> O.help "command")
+    <*> many (O.strArgument $ O.metavar ":arg" <> O.help "arguments")
+  where
+    mk cmd args = CmdExec (cmd :| args)
+
+strArgument :: IsString a => [O.Mod O.ArgumentFields a] -> O.Parser a
+strArgument = O.strArgument . mconcat 
 
 optsParser :: O.Parser Cmd
 optsParser = O.subparser $ mconcat
     [ cmdParser "build-docker" buildDockerOptions "Build docker images"
-    , cmdParser "packdeps" packdepsOptions "Run packdeps, i.e. check that dependency bounds allow newest versions"
-    , cmdParser "dot" dotOptions "Update dependency graph image"
-    , cmdParser "stats" statsOptions "Display some rough stats"
-    , cmdParser "estimator" estimatorOptions "Calculate estimates"
-    , cmdParser "view-config" viewConfigOptions "view config"
+    , cmdParser "packdeps"     packdepsOptions    "Run packdeps, i.e. check that dependency bounds allow newest versions"
+    , cmdParser "dot"          dotOptions         "Update dependency graph image"
+    , cmdParser "stats"        statsOptions       "Display some rough stats"
+    , cmdParser "estimator"    estimatorOptions   "Calculate estimates"
+    , cmdParser "view-config"  viewConfigOptions  "view config"
+    , cmdParser "gen-pass"     genPassOptions     "Generate passwords"
+    , cmdParser "list-keys"    listKeysOptions    "List all keys"
+    , cmdParser "add-key"      addKeyOptions      "Add new key"
+    , cmdParser "add-own-key"  addOwnKeyOptions   "Add own new key (not shared with others)"
+    , cmdParser "update-key"   updateKeyOptions   "Update existing key"
+    , cmdParser "delete-key"   deleteKeyOptions   "Delete key"
+    , cmdParser "run"          runOptions         "Run command through cabal new-run"
+    , cmdParser "exec"         execOptions        "Execute command in environment"
     ]
   where
     cmdParser :: String -> O.Parser Cmd -> String -> O.Mod O.CommandFields Cmd
@@ -66,11 +125,18 @@ optsParser = O.subparser $ mconcat
          O.command cmd $ O.info (O.helper <*> parser) $ O.progDesc desc
 
 main' :: Cmd -> IO ()
-main' (BuildDocker imgs) = buildDocker imgs
-main' (Action x)         = x
-main' ViewConfig         = do
+main' (CmdBuildDocker imgs) = buildDocker imgs
+main' (CmdAction x)         = x
+main' CmdGenPass            = cmdGenPass
+main' CmdViewConfig         = do
     cfg <- readConfig
     print $ ansiPretty $ flip M.renderMustache Null <$> cfg
+main' (CmdListKeys b)    = listKeys b
+main' (CmdAddKey k v)    = addKey True k v
+main' (CmdAddOwnKey k v) = addKey False k v
+main' (CmdUpdateKey k v) = updateKey k v
+main' (CmdDeleteKey k)   = deleteKey k
+main' (CmdExec args)     = cmdExec args
 
 defaultMain :: IO ()
 defaultMain = O.execParser opts >>= main'
