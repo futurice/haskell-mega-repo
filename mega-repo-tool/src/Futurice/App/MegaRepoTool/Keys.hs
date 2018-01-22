742c30fee938004392e0d292bca0fd409e5c6af6
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-01-22 15:19:29 +0200

    Implement key handling commands

--- mega-repo-tool/src/Futurice/App/MegaRepoTool/Keys.hs
+++ mega-repo-tool/src/Futurice/App/MegaRepoTool/Keys.hs
@@ -11,7 +11,7 @@ module Futurice.App.MegaRepoTool.Keys (
 
 import Control.Lens        (forOf_)
 import Control.Monad.Catch (handleIOError)
-import Data.Aeson.Compat   (FromJSON (..), decodeStrict, withText)
+import Data.Aeson.Compat   (FromJSON (..), decodeStrict, encode, withText)
 import Futurice.Prelude
 import Prelude ()
 import System.Directory    (getAppUserDataDirectory)
@@ -19,10 +19,10 @@ import System.Environment  (lookupEnv)
 import System.Exit         (exitFailure)
 import System.Exit.Lens    (_ExitFailure)
 import System.FilePath     ((</>))
-import System.IO           (stderr)
+import System.IO           (stderr, hPutStr)
 
-import qualified Data.ByteString as BS
-
+import qualified Data.ByteString           as BS
+import qualified Data.ByteString.Lazy      as LBS
 import qualified Data.Map.Strict           as Map
 import qualified Data.Text                 as T
 import qualified Data.Text.IO              as T
@@ -37,6 +37,9 @@ newtype Fingerprint = Fingerprint Text
 
 instance FromJSON Fingerprint where
     parseJSON = withText "Fingerprint" $ pure . Fingerprint . T.replace " " ""
+
+fprArg :: Fingerprint -> String
+fprArg (Fingerprint f) = T.unpack f
 
 data Storage = Storage
     { storageUser    :: Text
@@ -86,6 +89,34 @@ readStorage = do
             exitFailure
         decodeStrict contents
 
+writeStorageAll :: Storage -> Map Text Text -> IO ()
+writeStorageAll storage m = do
+    let args = "-e" : "-a" : concatMap recipient fprs
+    (ec, encrypted, err) <- ProcessBS.readProcessWithExitCode
+        "gpg2" args (LBS.toStrict contents)
+    forOf_ _ExitFailure ec $ \_ ->  do
+        BS.hPutStr stderr err
+        exitFailure
+    BS.writeFile (dir </> "secrets" </> "all.asc") encrypted
+  where
+    contents      = encode m
+    dir           = storageAppDir storage
+    fprs          = storageAllFprs storage
+    recipient fpr = ["-r", fprArg fpr ]
+
+writeStorageOwn :: Storage -> Map Text Text -> IO ()
+writeStorageOwn storage m = do
+    (ec, encrypted, err) <- ProcessBS.readProcessWithExitCode
+        "gpg2" ["-e", "-a", "-r", fprArg $ storageFpr storage ] (LBS.toStrict contents)
+    forOf_ _ExitFailure ec $ \_ ->  do
+        BS.hPutStr stderr err
+        exitFailure
+    BS.writeFile (dir </> "secrets" </> (T.unpack user ++ ".asc")) encrypted
+  where
+    contents = encode m
+    user     = storageUser storage
+    dir      = storageAppDir storage
+
 -------------------------------------------------------------------------------
 -- Commands
 -------------------------------------------------------------------------------
@@ -103,10 +134,60 @@ listKeys _ = do
         T.putStrLn $ T.justifyLeft 30 ' ' k <> " : " <> v
 
 addKey :: Bool -> Text -> Text -> IO ()
-addKey _ _ _ = putStrLn "not imlemented"
+addKey forAll key value = do
+    storage <- readStorage
+    let m = (if forAll then storageAll else storageOwn) storage
+    when (Map.member key m) $ fail $ "key " ++ show key ++ " is already added. Do you want to `update-key`?"
+
+    let m' = m & at key ?~ value
+    if forAll
+    then writeStorageAll storage m'
+    else writeStorageOwn storage m'
 
 updateKey :: Text -> Text -> IO ()
-updateKey _ _ = putStrLn "not implemented"
+updateKey key value = do
+    storage <- readStorage
+
+    if Map.member key (storageOwn storage)
+    then updateOwn storage
+    else updateAll storage
+  where
+    updateOwn storage = do
+        let m = storageOwn storage
+        unless (Map.member key m) $ do
+            hPutStr stderr $ "key " ++ show key ++ " doesn't exist."
+            exitFailure
+        let m' = m & ix key .~ value
+        writeStorageOwn storage m'
+
+    updateAll storage = do
+        let m = storageAll storage
+        unless (Map.member key m) $ do
+            hPutStr stderr $ "key " ++ show key ++ " doesn't exist."
+            exitFailure
+        let m' = m & ix key .~ value
+        writeStorageAll storage m'
 
 deleteKey :: Text -> IO ()
-deleteKey _ = putStrLn "not implemented"
+deleteKey key = do
+    storage <- readStorage
+
+    if Map.member key (storageOwn storage)
+    then deleteOwn storage
+    else deleteAll storage
+  where
+    deleteOwn storage = do
+        let m = storageOwn storage
+        unless (Map.member key m) $ do
+            hPutStr stderr $ "key " ++ show key ++ " doesn't exist."
+            exitFailure
+        let m' = m & at key .~ Nothing
+        writeStorageOwn storage m'
+
+    deleteAll storage = do
+        let m = storageAll storage
+        unless (Map.member key m) $ do
+            hPutStr stderr $ "key " ++ show key ++ " doesn't exist."
+            exitFailure
+        let m' = m & at key .~ Nothing
+        writeStorageAll storage m'
