86c247bf93bfc76cc0833d389af9a94cfc538b10
Toni Okuogume <toni.okuogume@futurice.com>
2018-02-28 14:45:14 +0200

    Refactor SortCriteria to it's own file

---
+++ checklist-app/src/Futurice/App/Checklist/Types/SortCriteria.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
+{-# LANGUAGE TypeOperators   #-}
+module Futurice.App.Checklist.Types.SortCriteria where
+
+import Futurice.Generics
+import Futurice.Prelude
+import Prelude ()
+
+-- | Helper type for stats page sorting
+data SortCriteria = SortByActive | SortByArchive | SortByBoth
+  deriving (Eq, Show, Bounded, Enum, Generic)
+
+deriveGeneric ''SortCriteria
+deriveVia [t| ToHttpApiData SortCriteria `Via` Enumica SortCriteria |]
+deriveVia [t| FromHttpApiData SortCriteria `Via` Enumica SortCriteria |]
+
+instance TextEnum SortCriteria where
+    type TextEnumNames SortCriteria = '["active", "archive", "both"]
+
+instance ToParamSchema SortCriteria where toParamSchema = enumToParamSchema
+instance ToSchema SortCriteria where declareNamedSchema = enumDeclareNamedSchema
