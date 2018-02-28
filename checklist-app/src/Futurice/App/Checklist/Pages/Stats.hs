ded3882fdfeeb8228c286f9c6309c62a03ffbf0e
Toni Okuogume <toni.okuogume@futurice.com>
2018-02-28 10:36:25 +0200

    Clean stats page code

--- checklist-app/src/Futurice/App/Checklist/Pages/Stats.hs
+++ checklist-app/src/Futurice/App/Checklist/Pages/Stats.hs
@@ -24,60 +24,61 @@ statsPage
     -> SortCriteria
     -> Bool
     -> HtmlPage "stats"
-statsPage world authUser sortCriteria sortDescEmpl =
-    let countUsers AnnTaskItemDone {} = Counter 1 1
-        countUsers AnnTaskItemTodo {} = Counter 0 1
-
-        sortByEmpl SortByActive  (SortedTask _ (Counter i j) _ _) = if j == 0 then 0 else fromIntegral i/ fromIntegral j :: Double
-        sortByEmpl SortByArchive (SortedTask _ _ (Counter i j) _) = if j == 0 then 0 else fromIntegral i/ fromIntegral j :: Double
-        sortByEmpl SortByBoth    (SortedTask _ _ _ (Counter i j)) = if j == 0 then 0 else fromIntegral i/ fromIntegral j :: Double
-
-        tasks' = world ^.. worldTasksSortedByName . folded
+statsPage world authUser sortCriteria sortDescEmpl = checklistPage_ "Stats" authUser $ do
+    let tasks' = world ^.. worldTasksSortedByName . folded
         archivedEmployees = world ^. worldArchive
-        tasksWithArchivedEmployees = swapMapMap $ DM.map (\(ArchivedEmployee _ tm) -> tm) archivedEmployees
-
         archivedEmployeesCount = length archivedEmployees
+
+        tasksWithArchivedEmployees = swapMapMap $ DM.map (\(ArchivedEmployee _ tm) -> tm) archivedEmployees
 
         findArchivedEmployeesForTask :: Identifier Task -> Counter
         findArchivedEmployeesForTask i = Counter
                                          (maybe 0 length (tasksWithArchivedEmployees ^? ix i))
                                          archivedEmployeesCount
 
-        sortedTask = fmap (\task -> let activeCount = (foldMapOf (worldTaskItems' . ix (task ^. identifier) . folded) countUsers world)
-                                        archiveCount = (findArchivedEmployeesForTask (task ^. identifier))
+        sortedTask = fmap (\task -> let activeCount = foldMapOf (worldTaskItems' . ix (task ^. identifier) . folded) countUsers world
+                                        archiveCount = findArchivedEmployeesForTask (task ^. identifier)
                                     in  SortedTask
                                         task
                                         activeCount
                                         archiveCount
                                         (activeCount <> archiveCount)) tasks'
-        sortedTask' = sortedTask & (if sortDescEmpl then sortOn $ Down . (sortByEmpl sortCriteria) else sortOn (sortByEmpl sortCriteria))
+        sortedTask' = sortedTask & (if sortDescEmpl then sortOn $ Down . sortByEmpl sortCriteria else sortOn (sortByEmpl sortCriteria))
 
-        showPercentageHtml i j = do
-            toHtml (show i)
-            "/"
-            toHtml (show j)
-            " = "
-            if j == 0 then "0" else toHtml (printf "%.01f" (100 * fromIntegral i / fromIntegral j :: Double) :: String)
-            "%"
+    header "Stats" []
+    row_ $ large_ 12 $ table_ $ do
+        thead_ $ tr_ $ do
+            th_ [title_ "Task"] "Task"
+            th_ [title_ "Active employees todo/done"] $
+                a_ [href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByActive (if sortCriteria == SortByActive then not sortDescEmpl else False)] "Active Empl"
+            th_ [title_ "Archived employees todo/done"] $
+                a_ [href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByArchive (if sortCriteria == SortByArchive then not sortDescEmpl else False)] "Archived Empl"
+            th_ [title_ "Combined employees todo/done"] $
+                a_ [href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByBoth (if sortCriteria == SortByBoth then not sortDescEmpl else False)] "Both"
 
-    in checklistPage_ "Stats" authUser $ do
+        tbody_ $ for_ sortedTask' $ \(SortedTask task counterActive counterArchive counterBoth) -> tr_ $ do
+            td_ $ taskLink task
+            td_ $ do
+                showPercentageHtml counterActive
+            td_ $ do
+                showPercentageHtml counterArchive
+            td_ $ do
+                showPercentageHtml counterBoth
 
-       header "Stats" []
-       row_ $ large_ 12 $ table_ $ do
-           thead_ $ tr_ $ do
-               th_ [title_ "Task"] "Task"
-               th_ [title_ "Active employees todo/done"] $
-                   a_ [href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByActive (if sortCriteria == SortByActive then not sortDescEmpl else False)] "Active Empl"
-               th_ [title_ "Archived employees todo/done"] $
-                   a_ [href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByArchive (if sortCriteria == SortByArchive then not sortDescEmpl else False)] "Archived Empl"
-               th_ [title_ "Combined employees todo/done"] $
-                   a_ [href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByBoth (if sortCriteria == SortByBoth then not sortDescEmpl else False)] "Both"
+showPercentageHtml :: Counter -> HtmlT Identity ()
+showPercentageHtml (Counter i j) = do
+    toHtml (show i)
+    "/"
+    toHtml (show j)
+    " = "
+    if j == 0 then "0" else toHtml (printf "%.01f" (100 * fromIntegral i / fromIntegral j :: Double) :: String)
+    "%"
 
-           tbody_ $ for_ sortedTask' $ \(SortedTask task (Counter i j) (Counter archi archj) (Counter bothi bothj)) -> tr_ $ do
-               td_ $ taskLink task
-               td_ $ do
-                   showPercentageHtml i j
-               td_ $ do
-                   showPercentageHtml archi archj
-               td_ $ do
-                   showPercentageHtml (bothi) (bothj)
+countUsers :: AnnTaskItem -> Counter
+countUsers AnnTaskItemDone {} = Counter 1 1
+countUsers AnnTaskItemTodo {} = Counter 0 1
+
+sortByEmpl :: SortCriteria -> SortedTask -> Double
+sortByEmpl SortByActive  (SortedTask _ (Counter i j) _ _) = if j == 0 then 0 else fromIntegral i/ fromIntegral j
+sortByEmpl SortByArchive (SortedTask _ _ (Counter i j) _) = if j == 0 then 0 else fromIntegral i/ fromIntegral j
+sortByEmpl SortByBoth    (SortedTask _ _ _ (Counter i j)) = if j == 0 then 0 else fromIntegral i/ fromIntegral j
