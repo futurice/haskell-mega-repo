c221fb8a886c2e36b20cc97a5a723bc4b0b30abe
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-02-28 09:28:51 +0200

    Use personio data for /power/users

--- reports-app/src/Futurice/App/Reports/PowerUser.hs
+++ reports-app/src/Futurice/App/Reports/PowerUser.hs
@@ -15,17 +15,19 @@ module Futurice.App.Reports.PowerUser (
     PowerUser (..),
     ) where
 
-import Prelude ()
-import Futurice.Prelude
+import Control.Lens            ((<&>))
 import Futurice.Generics
 import Futurice.Integrations
+import Futurice.Prelude
 import Futurice.Report.Columns
-import Data.Vector.Lens (toVectorOf)
+import Futurice.Tribe          (tribeToText)
+import Prelude ()
 
-import qualified PlanMill            as PM
-import qualified PlanMill.Queries    as PMQ
-import qualified Data.HashMap.Strict as HM
+import qualified Data.Map.Strict  as Map
+import qualified Data.Vector      as V
 import qualified FUM
+import qualified Personio         as P
+import qualified PlanMill         as PM
 
 -------------------------------------------------------------------------------
 -- Data
@@ -65,56 +67,34 @@ type PowerUserReport = Vector PowerUser
 -- Logic
 -------------------------------------------------------------------------------
 
-type SupervisorMap = HashMap FUM.Login (FUM.Login, Text)
+type EmployeeMap = Map P.EmployeeId P.Employee
 
-powerUserReport
-    :: forall m env.
-        ( PM.MonadTime m, MonadFUM m, MonadPlanMillQuery m
-        , MonadReader env m, HasFUMEmployeeListName env
-        )
-    => m PowerUserReport
+powerUserReport :: (PM.MonadTime m, MonadPersonio m) => m PowerUserReport
 powerUserReport = do
-    fpm <- snd <$$> fumPlanmillMap
-    supervisorMap <- supervisors <$> fumEmployeeList
-    toVectorOf folded <$> itraverse (powerUser supervisorMap) fpm
-  where
-    -- TODO: move into futurice-integrations, also used in balances
-    supervisors :: Vector FUM.User -> SupervisorMap
-    supervisors us = HM.fromList . mapMaybe mk $ us'
-      where
-        us' = toList us
+    today <- currentDay
+    es <- personio P.PersonioEmployees
+    let es' = Map.fromList $ map (\e -> (e ^. P.employeeId, e)) es
+    return $ V.fromList $ mapMaybe (powerUser today es') es
 
-        mk u = do
-          superId   <- u ^. FUM.userSupervisor . lazy
-          superData <- ss ^? ix superId
-          pure (u ^. FUM.userName, superData)
-
-        -- id to name map
-        ss :: HashMap Int (FUM.Login, Text)
-        ss = HM.fromList . map mkData $ us'
-
-        mkData :: FUM.User -> (Int, (FUM.Login, Text))
-        mkData u =
-            ( u ^. FUM.userId
-            , ( u ^. FUM.userName, u ^. FUM.userFullName)
-            )
-
-powerUser
-    :: MonadPlanMillQuery m
-    => SupervisorMap
-    -> FUM.Login -> PM.User -> m PowerUser
-powerUser supervisors fumLogin u = do
-    t <- traverse PMQ.team (PM.uTeam u)
-    a <- PMQ.enumerationValue (PM.uPassive u) "-"
+powerUser :: Day -> EmployeeMap -> P.Employee -> Maybe PowerUser
+powerUser today es e = do
+    login <- e ^. P.employeeLogin
     pure PowerUser
-        { _powerUserUsername       = fumLogin
-        , _powerUserFirst          = PM.uFirstName u
-        , _powerUserLast           = PM.uLastName u
-        , _powerUserTeam           = maybe "Unknown Team" PM.tName t
-        , _powerUserCompetence     = fromMaybe "No competence" (PM.uCompetence u)
-        , _powerUserStart          = PM.uHireDate u
-        , _powerUserEnd            = PM.uDepartDate u
-        , _powerUserActive         = a
-        , _powerUserSupervisor     = supervisors ^? ix fumLogin . _1
-        , _powerUserSupervisorName = supervisors ^? ix fumLogin . _2
+        { _powerUserUsername       = login
+        , _powerUserFirst          = e ^. P.employeeFirst
+        , _powerUserLast           = e ^. P.employeeLast
+        , _powerUserTeam           = tribeToText $ e ^. P.employeeTribe
+        , _powerUserCompetence     = e ^. P.employeeRole
+        , _powerUserStart          = e ^. P.employeeHireDate
+        , _powerUserEnd            = e ^. P.employeeEndDate
+        , _powerUserActive         =
+            if P.employeeIsActive today e
+            then "Active"
+            else "Passive"
+        , _powerUserSupervisor     = s >>= view P.employeeLogin
+        , _powerUserSupervisorName = s <&> view P.employeeFullname
         }
+  where
+    s = do
+        sid <- e ^. P.employeeSupervisorId
+        es ^? ix sid
