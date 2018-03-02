9cbe021e09a023f8ad9d89353cf733aeeec9112f
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-03-02 23:24:48 +0200

    Add mega-repo-tool run completer

--- mega-repo-tool/src/Futurice/App/MegaRepoTool.hs
+++ mega-repo-tool/src/Futurice/App/MegaRepoTool.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Futurice.App.MegaRepoTool (defaultMain) where
 
+import Cabal.Plan
+       (CompInfo (..), CompName (..), PkgId (..), PkgName (..), PlanJson (..),
+       Unit (..), dispCompName, findAndDecodePlanJson)
 import Data.Aeson                              (Value (Null))
+import Data.Maybe                              (isJust)
 import Futurice.Prelude
 import Prelude ()
 import Text.PrettyPrint.ANSI.Leijen.AnsiPretty (ansiPretty)
 
 import qualified Data.Map            as Map
+import qualified Data.Text           as T
 import qualified Options.Applicative as O
 import qualified Text.Microstache    as M
 
@@ -88,19 +94,98 @@ deleteKeyOptions = CmdDeleteKey
 
 runOptions :: O.Parser Cmd
 runOptions = cmdRun
-    <$> many (O.strArgument $ O.metavar ":arg" <> O.help "arguments")
+    <$> exe
+    <*> many (O.strArgument $ O.metavar ":arg" <> O.help "arguments")
   where
-    cmdRun args = CmdExec ("cabal" :| "new-run" : args)
+    cmdRun e args = CmdExec ("cabal" :| "new-run" : e : args)
+
+    exe = O.strArgument $ mconcat
+        [ O.metavar "exe"
+        , O.help "Patterns to match."
+        , O.completer $ patternCompleter True
+        ]
+
+patternCompleter :: Bool -> O.Completer
+patternCompleter onlyWithExes = O.mkCompleter $ \pfx -> do
+    (plan, _) <- findAndDecodePlanJson Nothing
+    let tpfx  = T.pack pfx
+        components = findComponents plan
+
+    -- One scenario
+    -- $ cabal-plan list-bin cab<TAB>
+    -- $ cabal-plan list-bin cabal-plan<TAB>
+    -- $ cabal-plan list-bin cabal-plan:exe:cabal-plan
+    --
+    -- Note: if this package had `tests` -suite, then we can
+    -- $ cabal-plan list-bin te<TAB>
+    -- $ cabal-plan list-bin tests<TAB>
+    -- $ cabal-plan list-bin cabal-plan:test:tests
+    --
+    -- *BUT* at least zsh script have to be changed to complete from non-prefix.
+    return $ map T.unpack $ firstNonEmpty
+        -- 1. if tpfx matches component exacty, return full path
+        [ single $ map fst $ filter ((tpfx ==) . snd) components
+
+        -- 2. match component parts
+        , uniques $ filter (T.isPrefixOf tpfx) $ map snd components
+
+        -- otherwise match full paths
+        , filter (T.isPrefixOf tpfx) $ map fst components
+        ]
+  where
+    firstNonEmpty :: [[a]] -> [a]
+    firstNonEmpty []         = []
+    firstNonEmpty ([] : xss) = firstNonEmpty xss
+    firstNonEmpty (xs : _)   = xs
+
+    -- single
+    single :: [a] -> [a]
+    single xs@[_] = xs
+    single _      = []
+
+    -- somewhat like 'nub' but drop duplicate names. Doesn't preserve order
+    uniques :: Ord a => [a] -> [a]
+    uniques = Map.keys . Map.filter (== 1) . Map.fromListWith (+) . map (\x -> (x, 1 :: Int))
+
+    impl :: Bool -> Bool -> Bool
+    impl False _ = True
+    impl True  x = x
+
+    -- returns (full, cname) pair
+    findComponents :: PlanJson -> [(T.Text, T.Text)]
+    findComponents plan = do
+        (_, Unit{..}) <- Map.toList $ pjUnits plan
+        (cn, ci) <- Map.toList $ uComps
+
+        -- if onlyWithExes, component should have binFile
+        guard (onlyWithExes `impl` isJust (ciBinFile ci))
+
+        let PkgId pn@(PkgName pnT) _ = uPId
+            g = case cn of
+                CompNameLib -> pnT <> T.pack":lib:" <> pnT
+                _           -> pnT <> T.pack":" <> dispCompName cn
+
+        let cnT = extractCompName pn cn
+        [ (g, cnT) ]
+
+extractCompName :: PkgName -> CompName -> T.Text
+extractCompName (PkgName pn) CompNameLib         = pn
+extractCompName (PkgName pn) CompNameSetup       = pn
+extractCompName _            (CompNameSubLib cn) = cn
+extractCompName _            (CompNameFLib cn)   = cn
+extractCompName _            (CompNameExe cn)    = cn
+extractCompName _            (CompNameTest cn)   = cn
+extractCompName _            (CompNameBench cn)  = cn
 
 execOptions :: O.Parser Cmd
-execOptions = mk 
+execOptions = mk
     <$> O.strArgument (O.metavar ":cmd" <> O.help "command")
     <*> many (O.strArgument $ O.metavar ":arg" <> O.help "arguments")
   where
     mk cmd args = CmdExec (cmd :| args)
 
 strArgument :: IsString a => [O.Mod O.ArgumentFields a] -> O.Parser a
-strArgument = O.strArgument . mconcat 
+strArgument = O.strArgument . mconcat
 
 optsParser :: O.Parser Cmd
 optsParser = O.subparser $ mconcat
