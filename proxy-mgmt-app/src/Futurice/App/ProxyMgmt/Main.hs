0c0b37cf0a9111627a347f8fd635a38ba6d78862
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-03-03 12:27:52 +0200

    proxy-mgmt markups

--- proxy-mgmt-app/src/Futurice/App/ProxyMgmt/Main.hs
+++ proxy-mgmt-app/src/Futurice/App/ProxyMgmt/Main.hs
@@ -8,34 +8,36 @@
 {-# LANGUAGE TypeOperators         #-}
 module Futurice.App.ProxyMgmt.Main (defaultMain) where
 
-import Futurice.Prelude
-import Prelude ()
-import Futurice.Integrations
-import Futurice.Postgres (createPostgresPool)
-import Futurice.Servant
-import Futurice.Lucid.Foundation (HtmlPage)
-import Servant
 import Dashdo.Servant
-import Futurice.FUM.MachineAPI (FUM6 (..), fum6)
-import FUM.Types.Login (Login)
+import FUM.Types.Login           (Login)
+import Futurice.FUM.MachineAPI   (FUM6 (..), fum6)
+import Futurice.Integrations
+import Futurice.Lucid.Foundation (HtmlPage)
+import Futurice.Postgres         (createPostgresPool)
+import Futurice.Prelude
+import Futurice.Servant
+import Prelude ()
+import Servant
 
-import qualified Data.Set        as Set
+import qualified Data.Set                   as Set
 import qualified Database.PostgreSQL.Simple as Postgres
 
 -- ProxyMgmt modules
+import Futurice.App.ProxyMgmt.AdminPage
 import Futurice.App.ProxyMgmt.API
-import Futurice.App.ProxyMgmt.Config (Config (..))
+import Futurice.App.ProxyMgmt.Config    (Config (..))
+import Futurice.App.ProxyMgmt.Ctx
 import Futurice.App.ProxyMgmt.Dashdo
 import Futurice.App.ProxyMgmt.IndexPage
-import Futurice.App.ProxyMgmt.Ctx
 
 server :: Ctx Identity -> Server ProxyMgmtAPI
-server ctx mfu = hoistServer proxyMgmtApi' (nt ctx mfu) $ 
-    indexPageHandler ctx
+server ctx =
+    (\mfu -> nt False ctx mfu $ indexPageHandler ctx)
+    :<|> (\mfu -> nt True ctx mfu $ adminPageHandler ctx)
 
 -- Access control adding transformation
-nt :: Ctx f -> Maybe Login -> IO a -> Handler a
-nt ctx mfu handler = do
+nt :: Bool -> Ctx f -> Maybe Login -> ReaderT Login IO a -> Handler a
+nt requireAdmin ctx mfu handler = do
     now <- currentTime
 
     -- Access control
@@ -44,15 +46,17 @@ nt ctx mfu handler = do
             fum6 $ FUMGroupEmployees (cfgAccessGroup cfg)
 
     case mfu <|> cfgMockUser cfg of
-        Just fu | Set.member fu fus -> liftIO $ handler
-        _                           -> throwError err403
+        Just fu | requireAdmin `implication` Set.member fu fus
+             -> liftIO $ runReaderT handler fu
+        _    -> throwError err403
   where
     cch = ctxCache ctx
     cfg = ctxConfig ctx
     lgr = ctxLogger ctx
     mgr = ctxManager ctx
 
-
+    implication False _ = True
+    implication True  x = x
 
 defaultMain :: IO ()
 defaultMain = futuriceServerMain (const makeCtx) $ emptyServerConfig
