d86f46ee198217a95509c62b3e7f623582ac35cd
Toni Okuogume <toni.okuogume@futurice.com>
2018-03-07 11:42:22 +0200

    Add option to hide checklistless tasks and show task roles

--- checklist-app/src/Futurice/App/Checklist/Markup.hs
+++ checklist-app/src/Futurice/App/Checklist/Markup.hs
@@ -234,7 +234,7 @@ personioPageHref :: Attribute
 personioPageHref = href_ $ linkToText $ safeLink checklistApi personioPageEndpoint
 
 statsPageHref :: Attribute
-statsPageHref = href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByActiveFuture False
+statsPageHref = href_ $ linkToText $ safeLink checklistApi statsPageEndpoint SortByActiveFuture False False
 
 -------------------------------------------------------------------------------
 -- Links
