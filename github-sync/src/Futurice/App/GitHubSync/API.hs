b67516a9ba779c09c0c1b1338c1eacd6a48dfece
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-03-26 15:57:53 +0300

    Add generic CommandResponse

--- github-sync/src/Futurice/App/GitHubSync/API.hs
+++ github-sync/src/Futurice/App/GitHubSync/API.hs
@@ -16,7 +16,7 @@ import qualified GitHub as GH
 type GitHubSyncAPI =
     SSOUser :> Get '[HTML] (HtmlPage "index")
     :<|> SSOUser :> "audit" :> Get '[HTML] (HtmlPage "audit")
-    :<|> SSOUser :> "command" :> "remove-users" :> ReqBody '[JSON] [GH.Name GH.User] :> Post '[JSON] Bool
+    :<|> SSOUser :> "command" :> "remove-users" :> ReqBody '[JSON] [GH.Name GH.User] :> Post '[JSON] (CommandResponse ())
 
 githubSyncApi :: Proxy GitHubSyncAPI
 githubSyncApi = Proxy
