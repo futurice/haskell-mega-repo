5ff4fe6487dff8da0f5a6021e25b1df68f07e273
Jussi Vaihia <jussiava@gmail.com>
2018-03-29 15:29:14 +0300

    PlanMill Hooks support (#545)
    read,add,delete hooks

--- planmill-client/src/PlanMill/Types/Inserted.hs
+++ planmill-client/src/PlanMill/Types/Inserted.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE DataKinds #-}
-module PlanMill.Types.Inserted (Inserted) where
+module PlanMill.Types.Inserted (
+    HookInserted,
+    Inserted)
+    where
 
 import Data.Aeson.Extra          (SingObject (..))
+import PlanMill.Types.Hook       (Hook)
 import PlanMill.Types.Identifier (Identifier)
 import PlanMill.Types.Timereport (Timereport)
 
 -- | Result type of 'addTimereport'.
 type Inserted i = SingObject "id" (Identifier Timereport)
+
+-- | Result type of 'addHook'.
+type HookInserted i = SingObject "id" (Identifier Hook)
