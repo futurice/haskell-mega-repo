f062afd0c9d6993a4f3f3d9cd8393c505c54a64d
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-04-27 12:21:41 +0300

    planmill-proxy capacities update lambda

--- planmill-proxy/src/Futurice/App/PlanMillProxy/Config.hs
+++ planmill-proxy/src/Futurice/App/PlanMillProxy/Config.hs
@@ -2,25 +2,19 @@ module Futurice.App.PlanMillProxy.Config (
     Config(..),
     ) where
 
-import Prelude ()
-import Futurice.Prelude
-import Database.PostgreSQL.Simple (ConnectInfo)
-import PlanMill                   (Cfg (..))
-
 import Futurice.EnvConfig
+import Futurice.Postgres  (ConnectInfo)
+import Futurice.Prelude
+import PlanMill           (Cfg (..))
+import Prelude ()
 
 data Config = Config
     { cfgCfg              :: !Cfg
     , cfgPostgresConnInfo :: !ConnectInfo
     }
-    deriving (Show)
+  deriving (Show)
 
 instance Configure Config where
-    configure = config
-        <$> envVar "PLANMILL_ADMIN"
-        <*> envVar "PLANMILL_SIGNATURE"
-        <*> envVar "PLANMILL_BASEURL"
+    configure = Config
+        <$> configure
         <*> envConnectInfo
-      where
-        config userid apikey baseurl =
-          Config (Cfg userid apikey baseurl)
