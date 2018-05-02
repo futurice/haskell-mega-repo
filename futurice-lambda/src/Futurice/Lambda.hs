8c89039523c804fbb5988952d78d72b7709b80be
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-05-02 11:21:46 +0300

    Lambda function_name and AWS.Env

--- futurice-lambda/src/Futurice/Lambda.hs
+++ futurice-lambda/src/Futurice/Lambda.hs
@@ -3,39 +3,53 @@
 module Futurice.Lambda (
     makeAwsLambda,
     AwsLambdaHandler,
+    LambdaContext (..),
+    -- * Re-exports
+    AWSEnv,
     ) where
 
 import Data.Aeson
        (FromJSON, ToJSON, Value, eitherDecodeStrict, encode, object, (.=))
-import Data.Aeson.Types    (emptyObject)
-import Foreign.C           (CString, withCString)
-import Foreign.Ptr         (Ptr)
-import Futurice.EnvConfig  (Configure, getConfig)
+import Data.Aeson.Types      (emptyObject)
+import Foreign.C             (CString, peekCString, withCString)
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Ptr           (Ptr)
+import Futurice.EnvConfig    (Configure, getConfig)
 import Futurice.Prelude
-import GHC.Stats           (getRTSStats)
-import Log                 (LogMessage (..), LogT, runLogT, showLogLevel)
-import Log.Internal.Logger (Logger (..))
-import Log.Monad           (LogT (..))
+import GHC.Stats             (getRTSStats)
+import Log                   (LogMessage (..), LogT, runLogT, showLogLevel)
+import Log.Internal.Logger   (Logger (..))
+import Log.Monad             (LogT (..))
 import Prelude ()
-import System.Environment  (getArgs)
+import System.Environment    (getArgs)
 
 import qualified Data.ByteString      as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text            as T
 import qualified Data.Text.Encoding   as TE
+import qualified Network.AWS.Env              as AWS
+import qualified Control.Monad.Trans.AWS      as AWS
 
 -- | A type for AWS Lambda Handler.
-type AwsLambdaHandler = CString -> Ptr LambdaContext -> Ptr LoggingFunc -> IO CString
+type AwsLambdaHandler = CString -> Ptr LambdaContextC -> Ptr LoggingFunc -> IO CString
+
+type AWSEnv = AWS.Env
 
 -- import some Python utilities
 foreign import ccall "PyObject_CallFunction" logC :: Ptr LoggingFunc -> CString -> CString -> IO (Ptr PyObject)
 foreign import ccall "Py_DecRef" pyDecRef :: Ptr PyObject -> IO ()
 
+foreign import ccall "futuriceLambdaFunctionName" futuriceLambdaFunctionName :: Ptr LambdaContextC -> IO CString
+
 -- LambdaContext
 -- https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html#python-context-object-methods
 --
 -- Currently unused.
-data LambdaContext
+data LambdaContextC
+
+data LambdaContext = LambdaContext
+    { lcName :: !Text
+    }
 
 -- | A special type for (callable) logging function.
 data LoggingFunc
@@ -46,19 +60,31 @@ data PyObject
 -- | Make an AWS Lambda handler.
 makeAwsLambda
     :: (FromJSON a, ToJSON b, NFData b, Configure cfg)
-    => (cfg -> Logger -> Manager -> a -> LogT IO b)
+    => (LambdaContext -> AWS.Env -> cfg -> Logger -> Manager -> a -> LogT IO b)
     -> AwsLambdaHandler
-makeAwsLambda handler input _ lf = do
-    inputBS  <- BS.packCString input
+makeAwsLambda handler input lc lf = do
+    -- LambdaContext
+    fnNameC <- futuriceLambdaFunctionName lc
+    fnName <- peekCString fnNameC
+    let fnNameT = fnName ^. packed
+    let lambdaContext = LambdaContext fnNameT
+
+    -- HTTP Manager
     mgr <- newManager tlsManagerSettings
-    output <- runLogT "lambda" lgr $ do
-        cfg <- getConfig "LAMBDA"
+
+    -- AWS environment
+    env' <- AWS.newEnvWith AWS.Discover Nothing mgr
+    let env = env' & AWS.envRegion .~ AWS.Frankfurt
+
+    inputBS  <- BS.packCString input
+    output <- runLogT fnNameT lgr $ do
+        cfg <- getConfig fnName
         case eitherDecodeStrict inputBS of
             Left err -> do
                 logAttention "Invalid JSON" err
                 encodeErr err
             Right x  -> do
-                res <- tryDeep' $ handler cfg lgr mgr x
+                res <- tryDeep' $ handler lambdaContext env cfg lgr mgr x
                 case res of
                     Left exc -> do
                         logAttention "Exception" $ show exc
