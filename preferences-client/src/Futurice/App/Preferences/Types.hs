3f628ad38bf03dc45630714081a697aaa9805cbd
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-06-04 22:48:12 +0300

    Add preferences-client

---
+++ preferences-client/src/Futurice/App/Preferences/Types.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
+{-# LANGUAGE TypeOperators   #-}
+module Futurice.App.Preferences.Types where
+
+import Futurice.Generics
+import Futurice.Prelude
+import Prelude ()
+
+data Preferences = Preferences
+    { _prefHoursPingSMS   :: !Bool
+    , _prefHoursPingEmail :: !Bool
+    }
+  deriving stock (Eq, Show, Generic)
+
+makeLenses ''Preferences
+deriveGeneric ''Preferences
+
+deriveVia [t| ToJSON Preferences   `Via` Sopica Preferences |]
+deriveVia [t| FromJSON Preferences `Via` Sopica Preferences |]
+
+instance ToSchema Preferences where declareNamedSchema = sopDeclareNamedSchema
