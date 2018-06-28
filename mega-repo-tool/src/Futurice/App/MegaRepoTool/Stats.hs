99bfba0ebef1ee01633ce1efa77e5ddefbd012b8
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-06-29 00:12:54 +0300

    more fun mega-repo-tool stats

--- mega-repo-tool/src/Futurice/App/MegaRepoTool/Stats.hs
+++ mega-repo-tool/src/Futurice/App/MegaRepoTool/Stats.hs
@@ -1,25 +1,24 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE BangPatterns  #-}
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Futurice.App.MegaRepoTool.Stats (
     stats,
     ) where
 
-import Prelude ()
-import Futurice.Prelude
 import Data.List                (isSuffixOf)
 import Data.Machine
 import Data.Machine.Runner      (foldT)
-import Data.Maybe               (fromJust)
 import Data.Semigroup           (Max (..))
 import Data.Semigroup.Generic   (gmappend, gmempty)
-import Data.TDigest             (TDigest, quantile, singleton)
+import Data.TDigest             (TDigest, singleton)
+import Data.TDigest.Postprocess (HistBin, histogram, quantile, cdf)
 import Futurice.Monoid          (Average (..))
+import Futurice.Prelude
+import Prelude ()
 import System.Directory.Machine (directoryWalk', files)
 import Text.Printf              (printf)
 
-import qualified Data.Text    as T
-import qualified Data.Text.IO as T
+import qualified Data.ByteString.Char8 as BS8
 
 -------------------------------------------------------------------------------
 -- Stats monoid
@@ -43,17 +42,17 @@ instance Monoid Stats where
     mempty = gmempty
     mappend = (<>)
 
-fileStats :: Text -> Stats
+fileStats :: ByteString -> Stats
 fileStats t = Stats
     { _statsFiles    = Sum $ 1
     , _statsLines    = Sum $ lineCount
-    , _statsNELines  = Sum $ length . filter (not . T.null) $ ls
+    , _statsNELines  = Sum $ length . filter (not . BS8.null) $ ls
     , _statsLinesAvg = Average 1 $ fromIntegral lineCount
     , _statsLinesMax = Max $ lineCount
     , _statsLinesTD  = singleton $ fromIntegral lineCount
     }
   where
-    ls        = T.lines t
+    ls        = BS8.lines t
     lineCount = length ls
 
 -------------------------------------------------------------------------------
@@ -66,7 +65,7 @@ statsMachine
     ~> files
     ~> filtered filePredicate
     -- ~> autoM (\x -> print x >> return x)
-    ~> autoM T.readFile
+    ~> autoM BS8.readFile
     ~> mapping fileStats
   where
     dirPredicate d = not . any ($ d) $
@@ -93,15 +92,40 @@ statsMachine
 stats :: IO ()
 stats = do
     Stats fs ls nels lavg lmax td <- foldT (statsMachine <~ source ["."])
+    let Just hist = histogram td
     putStrLn $ "total files:     " ++ show (getSum fs)
     putStrLn $ "total lines:     " ++ show (getSum ls)
     putStrLn $ "non-empty lines: " ++ show (getSum nels)
     putStrLn $ "average lines:   " ++ printf "%2.2f" (getAverage lavg)
-    putStrLn $ "line count 10%:  " ++ printf "%2.2f" (fromJust $ quantile 0.1 td)
-    putStrLn $ "line count 25%:  " ++ printf "%2.2f" (fromJust $ quantile 0.25 td)
-    putStrLn $ "line count 50%:  " ++ printf "%2.2f" (fromJust $ quantile 0.5 td)
-    putStrLn $ "line count 75%:  " ++ printf "%2.2f" (fromJust $ quantile 0.76 td)
-    putStrLn $ "line count 90%:  " ++ printf "%2.2f" (fromJust $ quantile 0.9 td)
-    putStrLn $ "line count 95%:  " ++ printf "%2.2f" (fromJust $ quantile 0.95 td)
-    putStrLn $ "line count 98%:  " ++ printf "%2.2f" (fromJust $ quantile 0.98 td)
+    putStrLn $ "line count 10%:  " ++ printf "%2.2f" (runIdentity $ quantile 0.1 hist)
+    putStrLn $ "line count 25%:  " ++ printf "%2.2f" (runIdentity $ quantile 0.25 hist)
+    putStrLn $ "line count 50%:  " ++ printf "%2.2f" (runIdentity $ quantile 0.5 hist)
+    putStrLn $ "line count 75%:  " ++ printf "%2.2f" (runIdentity $ quantile 0.76 hist)
+    putStrLn $ "line count 90%:  " ++ printf "%2.2f" (runIdentity $ quantile 0.9 hist)
+    putStrLn $ "line count 95%:  " ++ printf "%2.2f" (runIdentity $ quantile 0.95 hist)
+    putStrLn $ "line count 98%:  " ++ printf "%2.2f" (runIdentity $ quantile 0.98 hist)
     putStrLn $ "max lines:       " ++ show (getMax lmax)
+    traverse_ putStrLn $ plotTDigest hist
+
+plotTDigest :: NonEmpty HistBin -> [String]
+plotTDigest hist = map render $ dropLast $ map post buckets
+  where
+    mi = 0
+    ma = runIdentity (quantile 0.98 hist)
+
+    bucketMa = maximum (map snd buckets)
+    buckets = steps 0 mi
+    size = (ma - mi) / height
+    steps !acc x
+        | x >= ma   = []
+        | otherwise = (y, acc' - acc) : steps acc' y
+      where
+        y = x + size
+        acc' = cdf y hist
+
+    post (y, b) = (y, round (width * b / bucketMa))
+    dropLast = reverse . dropWhile ((== 0) . snd) . reverse
+    render (y, n) = printf "%8.2f" y ++ " " ++ replicate n '#'
+
+    height = 25
+    width = 50
