0248c5e2268d37b78a6c8a5ec13c016d6e994135
Toni Okuogume <toni.okuogume@futurice.com>
2018-07-02 17:05:24 +0300

    Prototype of library-app

---
+++ library-app/src/Futurice/App/Library/Config.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+module Futurice.App.Library.Config (
+    Config(..),
+    ) where
+
+import Futurice.EnvConfig
+import Futurice.Integrations
+import Futurice.Postgres
+import Futurice.Prelude
+import Prelude ()
+
+import qualified FUM.Types.Login as FUM
+
+data Config = Config
+    { cfgMockUser         :: !(Maybe FUM.Login)
+    , cfgPostgresConnInfo :: !ConnectInfo
+    , cfgIntegrationsCfg  :: !(IntegrationsConfig '[Proxy, Proxy, Proxy, Proxy, Proxy, I])
+    }
+
+instance Configure Config where
+    configure = Config
+        <$> optionalAlt (envVar "MOCKUSER")
+        <*> envConnectInfo
+        <*> configure
---
+++ library-app/src/Futurice/App/Library/Types/Library.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE InstanceSigs      #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
+module Futurice.App.Library.Types.Library where
+
+import Data.Aeson
+import Data.Swagger
+import Database.PostgreSQL.Simple.FromField
+
+import qualified Data.ByteString.Char8 as C
+import qualified Data.Text             as T
+
+import Futurice.Office
+import Futurice.Prelude
+import Prelude ()
+
+data Library
+    = OfficeLibrary Office
+    | Elibrary
+    | UnknownLibrary
+    deriving (Eq, Show, Ord, Typeable, Generic)
+
+deriveGeneric ''Library
+
+instance FromField Library where
+    fromField _ mdata = return library
+        where library =
+                  let officeText = T.pack <$> C.unpack <$> mdata >>= officeFromText
+                  in case officeText of
+                    Just office -> OfficeLibrary office
+                    Nothing -> case mdata of
+                      Just "Elibrary" -> Elibrary
+                      _ -> UnknownLibrary
+
+instance ToSchema Library where
+    declareNamedSchema _ = do
+        return $ NamedSchema (Just "Library") $ mempty
+            & type_ .~ SwaggerObject
+            & required .~ [ "office" ]
+
+instance ToJSON Library where
+    toJSON (OfficeLibrary office) = object ["office" .= office]
+    toJSON Elibrary               = object ["office" .= ("Elibrary" :: Text)]
+    toJSON _                      = object ["office" .= ("Unknown" :: Text)]
+
+instance FromJSON Library where
+    parseJSON = withObject "Library" $ \l -> do
+        office <- l .: "office"
+        case officeFromText office of
+          Just o -> pure $ OfficeLibrary o
+          Nothing -> if office == "Elibrary" then pure Elibrary else pure UnknownLibrary
