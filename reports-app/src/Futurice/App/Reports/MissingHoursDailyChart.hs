efc853ba8e947c2bba4e838bac43dcbefba90f40
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-07-13 17:50:37 +0300

    More missing hours series

--- reports-app/src/Futurice/App/Reports/MissingHoursDailyChart.hs
+++ reports-app/src/Futurice/App/Reports/MissingHoursDailyChart.hs
@@ -6,9 +6,10 @@ module Futurice.App.Reports.MissingHoursDailyChart (
     missingHoursDailyChartRender,
     ) where
 
-import Control.Lens                ((.=))
+import Control.Lens                ((.=), (^@..))
 import Futurice.Postgres
 import Futurice.Prelude
+import Futurice.Colour
 import Prelude ()
 import Servant.Chart               (Chart (..))
 
@@ -17,32 +18,37 @@ import Futurice.App.Reports.Ctx
 import qualified Data.Map.Strict               as Map
 import qualified Graphics.Rendering.Chart.Easy as C
 
-type MissingHoursDailyChartData = Map Day Double
+type MissingHoursDailyChartData = Map Day (Double, Double)
 
 missingHoursDailyChartData
     :: Ctx
     -> LogT IO MissingHoursDailyChartData
 missingHoursDailyChartData ctx = do
-    xs <- safePoolQuery_ ctx "SELECT day, hours FROM reports.missing_hours"
-    return (Map.fromList xs)
+    xs <- safePoolQuery_ ctx "SELECT day, last_friday, yesterday FROM reports.missing_hours"
+    return $ Map.fromList [ (k, (x, y)) | (k, x, y) <- xs ]
 
 missingHoursDailyChartRender :: MissingHoursDailyChartData -> Chart "missing-hours-daily"
 missingHoursDailyChartRender m = Chart . C.toRenderable $ do
     C.layout_title .= "Missing hours at each day"
-    C.plot $ line' lineStyle "missing hours" $ singleton $
-        Map.toList m
+    C.plot $ line' lineStyle1 "from two previous months until last Friday"
+        $ singleton $ m ^@.. ifolded . getter fst
+    C.plot $ line' lineStyle2 "from two previous months until yesteday"
+        $ singleton $ m ^@.. ifolded . getter snd
 
 singleton :: a -> [a]
 singleton x = [x]
 
 line' :: C.LineStyle -> String -> [[(x,y)]]  -> C.EC l (C.PlotLines x y)
 line' ls title values = C.liftEC $ do
-    C.plot_lines_title .= title
+    C.plot_lines_title  .= title
     C.plot_lines_values .= values
-    C.plot_lines_style .= ls
+    C.plot_lines_style  .= ls
 
-lineStyle :: C.LineStyle
-lineStyle = C.def
-    & C.line_color  .~ C.opaque C.red
-    & C.line_dashes .~ []
+lineStyle1 :: C.LineStyle
+lineStyle1 = C.def
+    & C.line_color  .~ C.opaque (colourToDataColour (FutuAccent AF4 AC3))
+
+lineStyle2 :: C.LineStyle
+lineStyle2 = C.def
+    & C.line_color  .~ C.opaque (colourToDataColour (FutuAccent AF2 AC3))
 
