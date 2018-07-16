fda410aa3989e3920a7e0b9918aeeb546f95bdd3
Toni Okuogume <toni.okuogume@futurice.com>
2018-07-16 17:19:57 +0300

    Updated database schema

--- library-app/schema.sql
+++ library-app/schema.sql
@@ -1,33 +1,10 @@
 CREATE SCHEMA library;
 
-CREATE TABLE library.library (
-    library_id serial primary key,
-    name text NOT NULL
-);
-
-CREATE TABLE library.item (
-    item_id serial primary key,
-    item_type text NOT NULL,
-    library_id integer references library.library(library_id)
-);
-
-CREATE TABLE library.old_loan (
-    oldloan_id serial primary key,
-    date_loaned date NOT NULL,
-    date_returned date NOT NULL,
-    personio_id integer NOT NULL,
-    item_id integer references library.item(item_id)
-);
-
-CREATE TABLE library.loan (
-    loan_id serial primary key,
-    date_loaned date NOT NULL,
-    personio_id integer NOT NULL,
-    item_id serial references library.item(item_id)
-);
+CREATE SEQUENCE library.loan_seq;
+CREATE SEQUENCE library.information_seq;
 
 CREATE TABLE library.bookinformation (
-    bookinfo_id serial primary key,
+    bookinfo_id integer primary key default nextval('library.information_seq'),
     title text NOT NULL,
     isbn text NOT NULL,
     author text NOT NULL,
@@ -37,19 +14,35 @@ CREATE TABLE library.bookinformation (
     amazon_link text
 );
 
-CREATE TABLE library.otherinformation (
-    otherinfo_id serial primary key,
-    name text NOT NULL
+CREATE TABLE library.boardgameinformation (
+    boardgameinfo_id integer primary key default nextval('library.information_seq'),
+    name text NOT NULL,
+    publisher text,
+    publishedYear smallint,
+    designer text,
+    artist text
 );
 
-CREATE TABLE library.item_to_bookinformation (
-    item_id serial references library.item(item_id),
-    bookinfo_id serial references library.bookinformation(bookinfo_id),
-    UNIQUE(item_id, bookinfo_id)
+CREATE TABLE library.item (
+    item_id serial primary key,
+    item_type text NOT NULL,
+    library text NOT NULL,
+    bookinfo_id integer references library.bookinformation(bookinfo_id),
+    boardgameinfo_id integer references library.boardgameinformation(boardgameinfo_id),
+    CONSTRAINT has_only_one_information_id CHECK ((bookinfo_id IS NULL)::integer + (boardgameinfo_id IS NULL)::integer = 1)
 );
 
-CREATE TABLE library.item_to_otherinformation (
-    item_id serial references library.item(item_id),
-    otherinfo_id serial references library.otherinformation(otherinfo_id),
-    UNIQUE(item_id, otherinfo_id)
+CREATE TABLE library.loan (
+    loan_id integer primary key default nextval('library.loan_seq'),
+    date_loaned date NOT NULL,
+    personio_id integer NOT NULL,
+    item_id integer NOT NULL references library.item(item_id)
+);
+
+CREATE TABLE library.old_loan (
+    oldloan_id integer primary key default nextval('library.loan_seq'),
+    date_loaned date NOT NULL,
+    date_returned date NOT NULL,
+    personio_id integer NOT NULL,
+    item_id integer NOT NULL references library.item(item_id)
 );
