0521759a0f894c7515f548ba30a6a1cd6de70903
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-09-20 22:51:07 +0300

    Timereports dump

--- reports-app/src/Futurice/App/Reports/TimereportsDump.hs
+++ reports-app/src/Futurice/App/Reports/TimereportsDump.hs
@@ -5,7 +5,6 @@ module Futurice.App.Reports.TimereportsDump (
     timereportsDump,
     ) where
 
-import Control.Monad.State.Lazy  (StateT, execStateT, modify)
 import Data.Aeson                (ToJSON (..))
 import FUM.Types.Login           (Login)
 import Futurice.Integrations
@@ -42,44 +41,41 @@ timereportsDump
     => m [SimpleTimereport]
 timereportsDump = do
     today <- currentDay
-    let interval = $(mkDay "2018-08-01") PM.... today
+    let interval = $(mkDay "2017-01-01") PM.... today
     let chopped = chopInterval interval
     us <- PMQ.users
-    consume $ bindForM_ (toList us) $ \pmUser -> do
-        trs <- lift $ for chopped $ \i -> do
-            -- we ask for all timereports
-            PMQ.timereports i (pmUser ^. PM.identifier)
-        for_ (PM.userLogin pmUser) $ \login -> do
-            for_ (mconcat trs) $ \tr -> do
-                for_ (PM.trProject tr) $ \prId -> do
-                    (p, t) <- lift $ liftA2 (,)
-                        (PMQ.project prId)
-                        (PMQ.task $ PM.trTask tr)
-                    produce SimpleTimereport
-                        { strLogin    = login
-                        , strProject  = PM.pName p
-                        , strTask     = PM.taskName t
-                        , strDay      = PM.trStart tr
-                        , strDuration = ndtConvert' $ PM.trAmount tr
-                        , strComment  = fromMaybe mempty $ PM.trComment tr
-                        }
-  where
-    consume :: Monad n => StateT ([a] -> [a]) n () -> n [a]
-    consume action = ($[]) <$> execStateT action id
+    data_ <- bindForM chopped $ \i ->
+        for (toList us) $ \pmUser -> do
+            for (PM.userLogin pmUser) $ \login -> do
+                -- we ask for all timereports
+                trs <- PMQ.timereports i (pmUser ^. PM.identifier)
+                for trs $ \tr -> do
+                    for (PM.trProject tr) $ \prId -> do
+                        (p, t) <- liftA2 (,)
+                            (PMQ.project prId)
+                            (PMQ.task $ PM.trTask tr)
+                        return SimpleTimereport
+                            { strLogin    = login
+                            , strProject  = PM.pName p
+                            , strTask     = PM.taskName t
+                            , strDay      = PM.trStart tr
+                            , strDuration = ndtConvert' $ PM.trAmount tr
+                            , strComment  = fromMaybe mempty $ PM.trComment tr
+                            }
 
-    produce :: Monad n => a -> StateT ([a] -> [a]) n ()
-    produce x = modify (. (x :))
+    return $ data_ ^.. folded  . folded . folded . folded . folded
 
 -------------------------------------------------------------------------------
 -- Extras
 -------------------------------------------------------------------------------
 
 -- bindForM and chopInterval used to cut the parallelism, as we ask "for everything"
-bindForM_ :: Monad m => [a] -> (a -> m b) -> m ()
-bindForM_ [] _ = return ()
-bindForM_ (a:as) f = do
-    _ <- f a
-    bindForM_ as f
+bindForM :: Monad m => [a] -> (a -> m b) -> m [b]
+bindForM xs f = go xs where
+    go [] = return []
+    go (a:as) = do
+        b <- f a
+        (b:) <$> go as
 
 chopInterval :: (Ord a, Enum a) => Interval a -> [Interval a]
 chopInterval i
