682e51b9fbe3eb56c25e34d489f753470963b37b
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-10-08 12:53:20 +0300

    Add Aux

--- reports-app/src/Futurice/App/Reports/ProjectHours.hs
+++ reports-app/src/Futurice/App/Reports/ProjectHours.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ApplicativeDo       #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE OverloadedStrings   #-}
@@ -14,19 +15,17 @@ module Futurice.App.Reports.ProjectHours (
     ProjectHours (..),
     ) where
 
-import Data.Tuple                (swap)
-import Futurice.Email            (Email)
+import Control.Lens              (alaf)
+import Data.Fixed                (Centi)
 import Futurice.Generics
 import Futurice.Integrations
 import Futurice.Lucid.Foundation
-import Futurice.Time.Month
-import Data.Fixed (Centi)
 import Futurice.Prelude
 import Futurice.Time
-import Numeric.Interval.NonEmpty (Interval, inf, sup, (...))
+import Futurice.Time.Month
+import Numeric.Interval.NonEmpty (Interval, (...))
 import Prelude ()
 
-import qualified Data.HashMap.Strict as HM
 import qualified Data.Map.Strict     as Map
 import qualified FUM.Types.Login     as FUM
 import qualified Personio            as P
@@ -70,17 +69,105 @@ instance ToSchema ProjectHoursData where declareNamedSchema = newtypeDeclareName
 projectHoursData
     :: forall m. (MonadTime m, MonadPersonio m, MonadPlanMillQuery m)
     => m ProjectHoursData
-projectHoursData = return $ PHD $ return
-    ProjectHours
-        { phLogin            = $(FUM.mkLogin "xxxx")
-        , phPersonio         = P.EmployeeId 0
-        , phBillableHours    = 0
-        , phNonBillableHours = 0
-        , phInternalHours    = 0
-        , phAbsenceHours     = 0
-        , phMonth            = dayToMonth $(mkDay "2017-01-01")
-        , phMonthCapacity    = 0
-        }
+projectHoursData = do
+    today <- currentDay
+    let interval = beginningOfPrev2Month today ... pred today
+    fpm <- personioPlanmillMap
+    PHD . fold <$> itraverse (\login (p, pm) -> perEmployee interval login p pm) fpm
+  where
+    perEmployee :: Interval Day -> FUM.Login -> P.Employee -> PM.User -> m [ProjectHours]
+    perEmployee interval login p pmu  = do
+        let uid = pmu ^. PM.identifier
+        trs <- PMQ.timereports interval uid
+
+        let grouped :: Map Month [PM.Timereport]
+            grouped = Map.fromListWith (++)
+                [ (dayToMonth (PM.trStart tr), [tr])
+                | tr <- toList trs
+                ]
+
+        fmap toList $ ifor grouped $ \month trs' -> do
+            uc <- PMQ.capacities (monthInterval month) uid
+            Aux {..} <- groupHours trs'
+            return ProjectHours
+                { phLogin            = login
+                , phPersonio         = p ^. P.employeeId
+                , phBillableHours    = auxBillable
+                , phNonBillableHours = auxNonBillable
+                , phInternalHours    = auxInternal
+                , phAbsenceHours     = auxAbsence
+                , phMonth            = month
+                , phMonthCapacity    = alaf Sum foldMap (ndtConvert' . PM.userCapacityAmount) uc
+                }
+
+    groupHours :: [PM.Timereport] -> m Aux
+    groupHours trs = fmap mconcat $ for trs $ \tr -> do
+        let hours = ndtConvert' $ PM.trAmount tr
+
+        -- for some reason annual holidays (at least?) have unknown billable status
+        billableStatus <- PMQ.enumerationValue (PM.trBillableStatus tr) "Non-billable"
+        dutyType <- traverse (`PMQ.enumerationValue` "???") (PM.trDutyType tr)
+
+        pid <- case PM.trProject tr of
+            Just pid -> return (Just pid)
+            Nothing  -> PM.taskProject <$> PMQ.task (PM.trTask tr)
+
+        mproject <- traverse PMQ.project pid
+
+        case status mproject billableStatus dutyType of
+            Billable       -> return mempty { auxBillable    = hours }
+            NonBillable    -> return mempty { auxNonBillable = hours }
+            Internal       -> return mempty { auxInternal    = hours }
+            Absence        -> return mempty { auxAbsence     = hours }
+            BalanceAbsence -> return mempty
+
+-------------------------------------------------------------------------------
+-- Aux
+-------------------------------------------------------------------------------
+
+data Aux = Aux
+    { auxBillable    :: NDT 'Hours Centi
+    , auxNonBillable :: NDT 'Hours Centi
+    , auxInternal    :: NDT 'Hours Centi
+    , auxAbsence     :: NDT 'Hours Centi
+    }
+
+instance Semigroup Aux where
+    Aux a b c d <> Aux a' b' c' d' =
+        Aux (a + a') (b +  b') (c + c') (d + d')
+
+instance Monoid Aux where
+    mempty  = Aux 0 0 0 0
+    mappend = (<>)
+
+data Status = Billable | NonBillable | Internal | Absence | BalanceAbsence
+  deriving (Eq, Show)
+
+-- in hours-api we assume project-id 0 if tasks' project id is not set.
+status
+    :: Maybe PM.Project
+    -> Text        -- ^ billable status
+    -> Maybe Text  -- ^ duty type
+    -> Status
+status _        "Non-billable" (Just "Balance leave") = BalanceAbsence
+status (Just p) "Non-billable" _  | isAbsence p       = Absence
+status (Just p) "Non-billable" _  | isInternal p      = Internal
+status _        "Non-billable" _                      = NonBillable
+status _        _              _                      = Billable
+
+-- | Absences go into magic project.
+--
+-- TODO: use enumeration
+isAbsence :: PM.Project -> Bool
+isAbsence p = PM.pCategory p == Just 900
+
+-- | TODO: Use PM.Account with type == 100
+-- or even better textual "My company"
+isInternal :: PM.Project -> Bool
+isInternal p = PM.pAccount p `elem`
+    [ Just (PM.Ident i)
+    | i <- [ 461507, 1003024, 3426, 17716, 469, 179108 ]
+    ]
 
 -------------------------------------------------------------------------------
 -- Html
@@ -105,7 +192,7 @@ renderProjectHoursData (PHD xs) = page_ "Project Hours" $ do
             th_ "Absence"
             th_ "Month Capacity"
 
-        tbody_ $ for_ xs $ \(ProjectHours {..}) -> tr_ $ do
+        tbody_ $ for_ xs $ \ProjectHours {..} -> tr_ $ do
             td_ $ toHtml phLogin
             td_ $ toHtml phPersonio
             td_ $ toHtml $ show phMonth
