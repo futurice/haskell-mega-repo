6b777a3d2a137e1a6d11d06d774d673adbf8426c
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-10-25 14:59:16 +0300

    More line styles

--- flowdock-proxy/src/Futurice/App/FlowdockProxy/Charts.hs
+++ flowdock-proxy/src/Futurice/App/FlowdockProxy/Charts.hs
@@ -5,10 +5,11 @@ module Futurice.App.FlowdockProxy.Charts (
     activityChart,
     ) where
 
-import Control.Lens     ((.=))
+import Control.Lens               ((.=))
+import Control.Monad.State.Strict (StateT, evalStateT, state)
 import Futurice.Prelude
 import Prelude ()
-import Servant.Chart    (Chart (..))
+import Servant.Chart              (Chart (..))
 
 import qualified Data.Map.Strict               as Map
 import qualified Graphics.Rendering.Chart.Easy as C
@@ -25,7 +26,7 @@ activityChart :: Map Text (Map Int Int) -> Chart "activity"
 activityChart xss = Chart . C.toRenderable $ do
     C.layout_title .= "Activity by (Finnish) hour (last 6 months)"
 
-    ifor_ xss $ \name xs -> do
+    flip evalStateT lineStyles $ ifor_ xss $ \name xs -> do
         let values' =
               [ (h, fromMaybe 0 $ Map.lookup h xs)
               | h <- [ 0 .. 23 ]
@@ -37,7 +38,29 @@ activityChart xss = Chart . C.toRenderable $ do
               | (h, x) <- values'
               ]
 
-        C.plot $ C.line (name ^. unpacked) [values]
+        style <- nextLineStyle
+        lift $ C.plot $ line' style (name ^. unpacked) [values]
 
     C.layout_x_axis . C.laxis_title .= "Hour"
     C.layout_y_axis . C.laxis_title .= "Relative activity"
+
+-- TODO: same as in MissingHoursChart
+line' :: C.LineStyle -> String -> [[(x,y)]]  -> C.EC l (C.PlotLines x y)
+line' lineStyle title values = C.liftEC $ do
+    C.plot_lines_title .= title
+    C.plot_lines_values .= values
+    C.plot_lines_style .= lineStyle
+
+lineStyles :: [C.LineStyle]
+lineStyles = cycle
+    [ C.def & C.line_color .~ C.opaque c & C.line_dashes .~ d
+    | d <- ds, c <- cs
+    ]
+  where
+    cs = [ C.red, C.blue, C.green, C.magenta, C.orange ]
+    ds = [[], [1,1], [5,5]]
+
+nextLineStyle ::  Monad m => StateT [C.LineStyle] m C.LineStyle
+nextLineStyle = state $ \s -> case s of
+    (x:xs) -> (x, xs)
+    []     -> (C.def, [])
