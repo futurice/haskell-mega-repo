1df96c391a8733a0c505c8f788bcc5958f45e04e
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-11-16 16:52:48 +0200

    Try gogol

---
+++ futurice-integrations/gogol-test/Test.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main (main) where
+
+import Futurice.Prelude
+import Network.Google
+import Network.Google.Auth   
+import Network.Google.People
+import Prelude ()
+import System.IO             (stdout)
+
+main :: IO ()
+main = do
+    mgr <- newManager tlsManagerSettings
+
+    -- todo: use fromFilePath
+    cred <- serviceAccountUser (Just "ogre@futurice.com") <$> getApplicationDefault mgr
+    case cred of
+        FromAccount _ -> putStrLn "FromAccount"
+        _ -> putStrLn "?"
+    lgr <- newLogger Debug stdout
+    env <- newEnvWith cred lgr mgr <&> (envScopes .~ contactsReadOnlyScope)
+    runResourceT $ runGoogle env $ do
+        x <- send (peopleConnectionsList "people/me")
+        liftIO $ print x
