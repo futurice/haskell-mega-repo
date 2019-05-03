4b83adccfbcc73123f795563c2bb713e708f4425
Toni Okuogume <toni.okuogume@futurice.com>
2019-05-03 14:43:55 +0300

    Add way to see groups and get all their members

--- okta-client/src/Okta/Types.hs
+++ okta-client/src/Okta/Types.hs
@@ -72,3 +72,46 @@ data User = User
     , userProfile :: !Profile
     } deriving (Show, GhcGeneric, SopGeneric, HasDatatypeInfo)
       deriving (ToJSON, FromJSON) via (Sopica User)
+
+data GroupType = OktaGroup
+               | AppGroup
+               | BuiltIn
+               deriving Show
+
+groupFromText :: Text -> Maybe GroupType
+groupFromText "OKTA_GROUP" = Just OktaGroup
+groupFromText "APP_GROUP"  = Just AppGroup
+groupFromText "BUILT_IN"   = Just BuiltIn
+groupFromText _            = Nothing
+
+groupToText :: GroupType -> Text
+groupToText OktaGroup = "OKTA_GROUP"
+groupToText AppGroup  = "APP_GROUP"
+groupToText BuiltIn   = "BUILT_IN"
+
+   -- "profile": {
+   --    "name": "West Coast Users",
+   --    "description": "All Users West of The Rockies"
+
+instance FromJSON GroupType where
+    parseJSON x = do
+        t <- parseJSON x
+        case groupFromText t of
+          Just g -> pure g
+          Nothing -> empty
+
+instance ToJSON GroupType where
+    toJSON = toJSON . groupToText
+
+data GroupProfile = GroupProfile
+    { profileName        :: !Text
+    , profileDescription :: !(Maybe Text)
+    } deriving (Show, GhcGeneric, SopGeneric, HasDatatypeInfo)
+      deriving (ToJSON, FromJSON) via (Sopica GroupProfile)
+
+data Group = Group
+    { groupId      :: !Text
+    , groupType    :: !GroupType
+    , groupProfile :: !GroupProfile
+    } deriving (Show, GhcGeneric, SopGeneric, HasDatatypeInfo)
+      deriving (ToJSON, FromJSON) via (Sopica Group)
