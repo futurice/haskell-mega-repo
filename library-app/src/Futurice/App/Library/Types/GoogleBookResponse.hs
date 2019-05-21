2435b8f5a5a735211fca919b603b575745a27734
Annaia Berry <jarcane@gmail.com>
2019-05-21 17:53:11 +0300

    Google Books response parse

---
+++ library-app/src/Futurice/App/Library/Types/GoogleBookResponse.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE InstanceSigs      #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
+module Futurice.App.Library.Types.GoogleBookResponse where
+
+import Data.Aeson
+import Futurice.Generics
+import Futurice.Prelude
+import Prelude ()
+
+data ISBNType 
+    = ISBN10
+    | ISBN13
+
+instance FromJSON ISBNType where
+    parseJSON (String "ISBN_10") = pure ISBN10
+    parseJSON (String "ISBN_13") = pure ISBN13
+    parseJSON _ = mzero
+
+data ISBN = ISBN
+    { _isbnType :: !ISBNType 
+    , _isbnIdentifier :: !Text
+    }
+
+instance FromJSON ISBN where
+    parseJSON (Object v) = do
+        t <- v .: "type"
+        idnum <- v .: "identifier"
+        pure $ ISBN t idnum
+    parseJSON _ = mzero
+
+data GoogleBookResponse = GoogleBookResponse
+    { _gbrTitle     :: !Text
+    , _gbrISBNs     :: ![ISBN]
+    , _gbrAuthors   :: ![Text]
+    , _gbrPublisher :: !Text
+    , _gbrPublished :: !Int
+    , _gbrBooksLink :: !Text
+    , _gbrCoverLink :: !Text
+    }
+
+instance FromJSON GoogleBookResponse where
+    parseJSON (Object o) = do
+        book <- head <$> o .: "items"
+        vi <- book .: "volumeInfo"
+        il <- vi .: "imageLinks"
+        GoogleBookResponse  <$> vi .: "title"
+                            <*> vi .: "industryIdentifiers"
+                            <*> vi .: "authors"
+                            <*> vi .: "publisher"
+                            <*> vi .: "publishedDate"
+                            <*> vi .: "canonicalVolumeLink"
+                            <*> il .: "thumbnail"
+    parseJSON _ = mzero
