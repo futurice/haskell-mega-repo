1f6bad3c98f829d64d06493d93b50c269885c8f5
Annaia Berry <jarcane@gmail.com>
2019-05-29 11:03:40 +0300

    Book info link should use href_ not src_

--- library-app/src/Futurice/App/Library/Pages/BookInformationPage.hs
+++ library-app/src/Futurice/App/Library/Pages/BookInformationPage.hs
@@ -30,7 +30,7 @@ bookInformationPage (BookInformationResponse binfoid title isbn author publisher
                 vertRow_ "Publisher" $ toHtml $ publisher
                 vertRow_ "Published" $ toHtml $ show published
                 vertRow_ "ISBN" $ toHtml $ isbn
-                vertRow_ "Info link" $ a_ [ src_ infoLink ] $ toHtml $ truncLink
+                vertRow_ "Info link" $ a_ [ href_ infoLink ] $ toHtml $ truncLink
             a_ [class_ "button small", href_ $ linkToText $ fieldLink editBookPageGet binfoid] "Edit book information"
     fullRow_ $ do
         h2_ "Books"
