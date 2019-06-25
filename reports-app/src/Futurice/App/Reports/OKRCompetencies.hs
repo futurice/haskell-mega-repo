d921eae830a3322d9177b27ee03156a07b90dbac
Annaia Berry <jarcane@gmail.com>
2019-06-25 16:05:13 +0300

    planmill->personio employee lookup

---
+++ reports-app/src/Futurice/App/Reports/OKRCompetencies.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE ApplicativeDo       #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+module Futurice.App.Reports.OKRCompetencies where
+
