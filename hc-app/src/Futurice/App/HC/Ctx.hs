5d5bf83ae9c573224c08411b39098837e153da85
Toni Okuogume <toni.okuogume@futurice.com>
2020-07-08 16:24:01 +0300

    Add database logging for send emails

--- hc-app/src/Futurice/App/HC/Ctx.hs
+++ hc-app/src/Futurice/App/HC/Ctx.hs
@@ -1,13 +1,18 @@
 module Futurice.App.HC.Ctx where
 
-import Prelude ()
+import Futurice.Postgres
 import Futurice.Prelude
+import Prelude ()
 
 import Futurice.App.HC.Config
 
 data Ctx = Ctx
-    { ctxConfig  :: Config
-    , ctxLogger  :: Logger
-    , ctxManager :: Manager
-    , ctxSecret  :: ByteString
+    { ctxConfig   :: !Config
+    , ctxLogger   :: !Logger
+    , ctxManager  :: !Manager
+    , ctxSecret   :: !ByteString
+    , ctxPostgres :: !(Pool Connection)
     }
+
+instance HasPostgresPool Ctx where
+    postgresPool = ctxPostgres
