3ffa44ff33e1a78b8a299842efaeb6d1d9ea7eb2
Toni Okuogume <neurogoo@users.noreply.github.com>
2020-12-31 09:42:58 +0200

    Early caring 2020 12 29 (#863)
    Update template and don't show empty emails

--- hc-app/src/Futurice/App/HC/EarlyCaring/Template.hs
+++ hc-app/src/Futurice/App/HC/EarlyCaring/Template.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell   #-}
-module Futurice.App.HC.EarlyCaring.Template (renderTemplate) where
+module Futurice.App.HC.EarlyCaring.Template (renderTemplate, emptyTemplate) where
 
 import Data.Aeson                (object, (.=))
-import Data.FileEmbed            (embedStringFile, makeRelativeToProject)
 import Data.Fixed                (Deci)
 import Futurice.Prelude
 import Futurice.Time
-import Futurice.Time.Month       (Month, dayToMonth)
+import Futurice.Time.Month       (dayToMonth)
 import Numeric.Interval.NonEmpty (Interval, inf)
 import Prelude ()
 import Text.Microstache          (Template, compileMustacheText, renderMustache)
@@ -17,6 +16,14 @@ import qualified Data.Text as T
 import qualified Personio  as P
 
 import Futurice.App.HC.EarlyCaring.Types
+
+-- | Will the template be empty based on template rules
+emptyTemplate :: Day -> Interval Day -> [Balance] -> Bool
+emptyTemplate today interval balances = null flex && null sickCount && null sickDays
+  where
+    flex = mapMaybe (mkBalance today interval) balances
+    sickCount = mapMaybe (mkCount today) balances
+    sickDays = mapMaybe (mkDays today) balances
 
 renderTemplate
     :: Text      -- ^ supervisor name
@@ -34,45 +41,49 @@ renderTemplate name today interval balances = renderMustache earlyCaringTemplate
     , "hasSick"     .= not (null sickCount && null sickDays)
     ]
   where
+    flex = mapMaybe (mkBalance today interval) balances
+    sickCount = mapMaybe (mkCount today) balances
+    sickDays = mapMaybe (mkDays today) balances
+
+monthToObject :: Balance -> Month -> Value
+monthToObject b m  = case balanceMonthFlex b ^. ix m of
+  MonthFlex marked capa -> object
+      [ "month"  .= m
+      , "diff"   .= formatHours (marked - capa)
+      , "marked" .= formatHours' marked
+      , "capa"   .= formatHours' capa
+      ]
+
+mkCount :: Day -> Balance -> Maybe Value
+mkCount today b
+    | balanceNormalAbsences today b = Nothing
+    | otherwise = Just $ object
+      [ "name"    .= T.justifyLeft 25 '.' (balanceEmployee b ^. P.employeeFullname)
+      , "cases"   .= countAbsences today (balanceAbsences b)
+      ]
+
+mkBalance :: Day -> Interval Day -> Balance -> Maybe Value
+mkBalance today interval b
+    | balanceNormalFlex today b = Nothing
+    | otherwise = Just $ object
+      [ "name"    .= (balanceEmployee b ^. P.employeeFullname)
+      , "balance" .= formatHours (balanceHours b)
+      , "missing" .= formatHours (balanceMissingHours b)
+      , "sum"     .= formatHours (balanceHours b + balanceMissingHours b)
+      , "month1"  .= monthToObject b month1
+      , "month2"  .= monthToObject b month2
+      ]
+  where
     month1 = dayToMonth $ inf interval
     month2 = succ month1
 
-    flex = mapMaybe mkBalance balances
-    mkBalance b
-        | balanceNormalFlex today b = Nothing
-        | otherwise = Just $ object
-            [ "name"    .= (balanceEmployee b ^. P.employeeFullname)
-            , "balance" .= formatHours (balanceHours b)
-            , "missing" .= formatHours (balanceMissingHours b)
-            , "sum"     .= formatHours (balanceHours b + balanceMissingHours b)
-            , "month1"  .= monthToObject b month1
-            , "month2"  .= monthToObject b month2
-            ]
-
-    monthToObject :: Balance -> Month -> Value
-    monthToObject b m  = case balanceMonthFlex b ^. ix m of
-        MonthFlex marked capa -> object
-            [ "month"  .= m
-            , "diff"   .= formatHours (marked - capa)
-            , "marked" .= formatHours' marked
-            , "capa"   .= formatHours' capa
-            ]
-
-    sickCount = mapMaybe mkCount balances
-    mkCount b
-        | balanceNormalAbsences today b = Nothing
-        | otherwise = Just $ object
-            [ "name"    .= T.justifyLeft 25 '.' (balanceEmployee b ^. P.employeeFullname)
-            , "cases"   .= countAbsences today (balanceAbsences b)
-            ]
-
-    sickDays = mapMaybe mkDays balances
-    mkDays b
-        | balanceNormalAbsenceDays today b = Nothing
-        | otherwise = Just $ object
-            [ "name"    .= T.justifyLeft 25 '.' (balanceEmployee b ^. P.employeeFullname)
-            , "days"    .= countAbsenceDays today (balanceAbsences b)
-            ]
+mkDays :: Day -> Balance -> Maybe Value
+mkDays today b
+    | balanceNormalAbsenceDays today b = Nothing
+    | otherwise = Just $ object
+      [ "name"    .= T.justifyLeft 25 '.' (balanceEmployee b ^. P.employeeFullname)
+      , "days"    .= countAbsenceDays today (balanceAbsences b)
+      ]
 
 formatHours :: NDT 'Hours Deci -> Text
 formatHours n = sign <> formatHours' n where
